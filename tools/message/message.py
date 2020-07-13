baf088669dae9d3c45114789a31cb63025f143a9
Karpovich_alex_mpc <karpovehalex@gmail.com>
2020-07-14 00:47:44 +0300

    New message structure

--- tools/message/message.py
+++ tools/message/message.py
@@ -1,34 +1,41 @@
 from time import time
 import json
 import pickle
+from typing import Tuple
 
 
-class Message:
-    def __init__(self, from_user=None, to_user=None, m_type=None, m_content=None,
-                 **kwargs):
-        """
-        Create a message instance which helps to communicate client with server
-        :param from_user:
-        :param to_user:
-        :param m_type:
-        :param m_content:
-        :param kwargs:
-        """
-        self.m_type = m_type
+class DataContainer:
+    """
+    This class is used to be Parent for all messages and data, which will be sending via websockets
+
+    """
+
+    def __init__(self, data_type=None, from_user=None, to_user=None, **kwargs):
+        self._data_types = tuple()
+        self.data_type = data_type
         self.from_user = from_user
         self.to_user = to_user
-        self.content = m_content
-        self.m_created_time = time()
-        # self.m_send_time = None
+        self.created_time = time()
         for name, value in kwargs.items():
             self.__setattr__(name, value)
+
+    @property
+    def data_type(self):
+        return self._data_types
+
+    @data_type.setter
+    def data_type(self, val):
+        self._data_types: Tuple = self._data_types + (val,)
 
     def dump_to_json(self):
         return json.dumps(self.__dict__)
 
     def to_binary(self):
-        # self.m_send_time = time()
         return pickle.dumps(self.dump_to_json())
+
+    def prepare_message(self, from_user):
+        self.from_user = from_user
+        return self.to_binary()
 
     @classmethod
     def load_from_json(cls, json_file):
@@ -39,6 +46,50 @@ class Message:
     def from_binary(cls, binary_json):
         simple_json = pickle.loads(binary_json)
         return cls.load_from_json(simple_json)
+
+
+class Message(DataContainer):
+    """
+    A message instance which helps to communicate client with server
+    """
+
+    def __init__(self, from_user=None, to_user=None, message_type=None, message_text=None,
+                 **kwargs):
+        super().__init__(data_type='message', from_user=from_user, to_user=to_user)
+        self.message_type = message_type
+        self.message_text = message_text
+        for name, value in kwargs.items():
+            self.__setattr__(name, value)
+
+
+class ServiceDataContainer(DataContainer):
+    """
+    Use to sending information to server
+    """
+
+    def __init__(self, data_type=None, **kwargs):
+        super().__init__(data_type='service_message', to_user='server')
+        self.data_type = data_type
+
+
+class LoginData(ServiceDataContainer):
+    """
+    A Login instance using to authentication on server
+    use prepare_data method to add from user field
+    """
+
+    def __init__(self, login=None, password=None, **kwargs):
+        super().__init__(data_type='login_data')
+        self.login = login
+        self.password = password
+
+
+class SignInData(ServiceDataContainer):
+    def __init__(self, nickname=None, login=None, password=None, **kwargs):
+        super().__init__(data_type='sign_in')
+        self.nickname = nickname
+        self.login = login
+        self.password = password
 
 
 if __name__ == "__main__":
